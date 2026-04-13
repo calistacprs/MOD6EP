0f023b4b657a6ea8079fa28956f0d392d84e2718
bonbernido <menediobernido@gmail.com>
2026-04-13 16:35:04 +0800

    removed managedaccount w/o int

--- M6E-DjangoForms-BernidoCaparasTuazon /tapas/tapasapp/urls.py
+++ M6E-DjangoForms-BernidoCaparasTuazon /tapas/tapasapp/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('', views.view_login, name='login'),
     path('signup/', views.view_signup, name='signup'),
     path('basic_list/<int:pk>/', views.view_basic_list, name='basic_list'),
-    path('manage_account/', views.manage_account, name='manage_account'),
     path('logout/', views.logout, name='logout'),
     path('manage_account/<int:pk>/', views.manage_account, name='manage_account'),
     path('change_password/<int:pk>/', views.change_password, name='change_password'),
